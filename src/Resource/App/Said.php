647ef3b7fb817778183bb08a41a73a2746ab7af9
kin29 <kin29.com@gmail.com>
2018-04-21 21:39:26 +0900

    fetchAllで複数レコードとれた！他、インデント調整

--- src/Resource/App/Said.php
+++ src/Resource/App/Said.php
@@ -10,34 +10,33 @@ class Said extends ResourceObject
 
     public function onGet(string $mode, int $id = null, string $said = null, string $who = null) : ResourceObject
     {
-        switch($mode){
+        switch ($mode) {
             //新規登録
             case 'insert':
-                $this->body = 'insertしました';
                 $this
                     ->db
                     ->insert(
                         'said_table',
-                         ['said' => $said, 'who' => $who, 'create_date' => new \DateTime('now'), 'del_flg' => 0],
-                         array('create_date' => 'datetime')
+                        ['said' => $said, 'who' => $who, 'create_date' => new \DateTime('now'), 'del_flg' => 0],
+                        array('create_date' => 'datetime')
                     );
                 // created
                 $this->code = 201;
-
+                $this->body = "insert[$said]";
                 break;
 
             //削除
             case 'delete':
-                $this->body = 'deleteしました';
-                 $this
+                $this
                     ->db
                     ->update(
-                  'said_table',
+                        'said_table',
                         ['del_flg' => 1],
                         ['id' => $id]
                     );
                 // no content
                 $this->code = 204;
+                $this->body = "delete!(id=$id)";
                 break;
 
             //一覧取得
@@ -45,20 +44,19 @@ class Said extends ResourceObject
                 $this->body = $this
                     ->db
                     ->newQuery()
-                    ->select('*')
+                    ->select('id, said, who, create_date')
                     ->from('said_table')
                     ->where('del_flg = 0')
                     ->execute()
-                    ->fetch('assoc');
-
+                    ->fetchAll('assoc');
                 break;
 
             //ランダムな名言を取得
             case 'random':
-            $this->body = $this
+                $this->body = $this
                     ->db
                     ->newQuery()
-                    ->select('*')
+                    ->select('id, said, who')
                     ->from('said_table')
                     ->where('del_flg = 0')
                     ->order('RANDOM()')
@@ -66,7 +64,8 @@ class Said extends ResourceObject
                     ->fetch('assoc');
 
                 break;
-
+            default;
+                $this->body = "not found mode[mode=$mode]";
         }
         return $this;
     }
